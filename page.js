5dc01eec0b5a869f79e8355b57658d91e3854c74
kenshinji <gundam0083ster@gmail.com>
2016-11-04 13:12:06 +0800

    Add missing style directory

--- page.js
+++ page.js
@@ -5,6 +5,18 @@ $(document).ready(function(){
     // $("#container").html($("#trackNumber").text());
     console.log("search clicked");
     $("#container").html("查询的快递号：" + $("#trackNumber").val());
+    $("#search").click(function(e){
+      e.preventDefault();
+      $.ajax({
+        type:"GET",
+        async:true,
+        url:"http://www.kuaidi100.com/query?type=shunfeng",
+        data:{postid:$("#trackNumber").val()},
+        success:function(data) {
+          // DO SOMETHING
+              console.log(data);
+            }
+      });
+    });
   });
-
 });
