0cd23fa8431f75aea8a13bf7813fe58ed3b8fcd2
gongzhou <sean.gongz@gmail.com>
2016-11-04 14:53:24 +0100

    直接显示查询返回结果。

--- page.js
+++ page.js
@@ -1,12 +1,12 @@
 console.log("start of page.js");
 $(document).ready(function(){
   console.log("document is ready.");
-  $("#search").click(function(){
+  $("#search").click(function(e){
     // $("#container").html($("#trackNumber").text());
     //for test, we can use type = shunfeng and postid = 608582127345
     console.log("search clicked");
-    $("#container").html("查询的快递号：" + $("#trackNumber").val());
-    $("#search").click(function(e){
+    $("#container").html("正在查询的快递号：" + $("#trackNumber").val()+" ...");
+    // $("#search").click(function(e){
       e.preventDefault();
       $.ajax({
         type:"GET",
@@ -16,8 +16,9 @@ $(document).ready(function(){
         success:function(data) {
           // DO SOMETHING
               console.log(data);
+              $("#container").html(data);
             }
       });
-    });
+    // });
   });
 });
